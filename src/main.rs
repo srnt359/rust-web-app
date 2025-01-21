b5bdfd41711631bf2d879f98fbe83d104b7e8f19
k.h <hazumi6@gmail.com>
2025-01-21 19:00:32 +0900

    render 公開設定

--- src/main.rs
+++ src/main.rs
@@ -99,7 +99,7 @@ async fn main() -> std::io::Result<()> {
             .service(update)
             .app_data(web::Data::new(pool.clone()))
     })
-    .bind("127.0.0.1:8080")?
+    .bind(("0.0.0.0", 10000))?
     .run()
     .await
 }
