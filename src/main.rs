fea9680cea76dcf3a073534fb0a997f1e8f4ad35
srnt359 <hazumi6@gmail.com>
2025-01-21 18:49:45 +0900

    initial commit

---
+++ src/main.rs
@@ -0,0 +1,105 @@
+use actix_web::{get, post, web, App, HttpResponse, HttpServer};
+use askama::Template;
+use askama_actix::TemplateToResponse;
+
+use sqlx::{Row, SqlitePool};
+
+#[derive(serde::Deserialize)]
+struct Task {
+    id: Option<String>,
+    task: Option<String>,
+}
+
+#[post("/update")]
+async fn update(pool: web::Data<SqlitePool>, form: web::Form<Task>) -> HttpResponse {
+    let task = form.into_inner();
+
+    if let Some(id) = task.id {
+        sqlx::query("DELETE FROM tasks WHERE task = ?")
+            .bind(id)
+            .execute(pool.as_ref())
+            .await
+            .unwrap();
+
+    }
+
+    match task.task {
+        Some(task)if !task.is_empty() => {
+            sqlx::query("INSERT INTO tasks (task) VALUES (?)")
+                .bind(task)
+                .execute(pool.as_ref())
+                .await
+                .unwrap();
+        }
+        _ => {}
+    }
+    HttpResponse::Found().append_header(("Location", "/")).finish()
+}
+
+#[derive(Template)]
+#[template(path = "hello.html")]
+struct HelloTemplate {
+    name: String,
+}
+
+#[get("/hello/{name}")]
+async fn hello(name: web::Path<String>) -> HttpResponse {
+    let hello = HelloTemplate {
+        name: name.into_inner(),
+    };
+    hello.to_response()
+}
+
+#[derive(Template)]
+#[template(path = "todo.html")]
+struct TODOTemplate {
+    tasks: Vec<String>,
+}
+
+#[get("/")]
+async fn todo(pool: web::Data<SqlitePool>) -> HttpResponse {
+    let rows = sqlx::query("SELECT task FROM tasks;")
+        .fetch_all(pool.as_ref())
+        .await
+        .unwrap();
+
+    let tasks: Vec<String> = rows
+        .iter()
+        .map(|row| row.get::<String, _>("task"))
+        .collect();
+
+    let todo = TODOTemplate { tasks };
+    todo.to_response()
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let pool = SqlitePool::connect("sqlite::memory:").await.unwrap();
+    sqlx::query("CREATE TABLE tasks (task Text) ")
+        .execute(&pool)
+        .await
+        .unwrap();
+    sqlx::query("INSERT INTO tasks (task) VALUES ('task1')")
+        .execute(&pool)
+        .await
+        .unwrap();
+    sqlx::query("INSERT INTO tasks (task) VALUES ('task2')")
+        .execute(&pool)
+        .await
+        .unwrap();
+    sqlx::query("INSERT INTO tasks (task) VALUES ('task3')")
+        .execute(&pool)
+        .await
+        .unwrap();
+
+    HttpServer::new(move || {
+        App::new()
+            .service(hello)
+            .service(todo)
+            .service(update)
+            .app_data(web::Data::new(pool.clone()))
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
